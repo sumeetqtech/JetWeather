de49acc76a25d43ef4e2585fe277b6aa7d02549e
Sumeet A. Panchal <sumeet@pharmeasy.in>
2025-02-26 13:34:40 +0530

    config file changes

--- app/src/main/java/com/compose/weatherforecast/ui/screens/about/AboutScreen.kt
+++ app/src/main/java/com/compose/weatherforecast/ui/screens/about/AboutScreen.kt
@@ -3,12 +3,7 @@ package com.compose.weatherforecast.ui.screens.about
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -16,7 +11,6 @@ import androidx.navigation.NavController
 import com.compose.weatherforecast.R
 import com.compose.weatherforecast.ui.components.WeatherTopBar
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(navController: NavController) {
     Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
--- app/src/main/java/com/compose/weatherforecast/util/Constants.kt
+++ app/src/main/java/com/compose/weatherforecast/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.compose.weatherforecast.util
 
 object Constants {
-    const val BASE_URL = "https://api.openweathermap.org/";
+    const val BASE_URL = "https://api.openweathermap.org/"
     const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
 }
